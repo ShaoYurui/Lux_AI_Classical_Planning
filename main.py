8f3564e05d857544b0e79bb4d42946ab7886754c
Yurui <69446663+ShaoYurui@users.noreply.github.com>
2023-04-05 23:26:24 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 import os
 import pyperplan
 
-GRID_WORLD_WIDTH = 10
-GRID_WORLD_HEIGHT = 10
-GOLD_MINE_POS = (2, 5)
+GOLD_MINE_POS = (22, 35)
 ROBOT_POS = (6, 3)
 
+GRID_WORLD_X_MIN = min(GOLD_MINE_POS[0], ROBOT_POS[0])
+GRID_WORLD_X_MAX = max(GOLD_MINE_POS[0], ROBOT_POS[0])
+GRID_WORLD_Y_MIN = min(GOLD_MINE_POS[1], ROBOT_POS[1])
+GRID_WORLD_Y_MAX = max(GOLD_MINE_POS[1], ROBOT_POS[1])
 domain_file = "./pddl/domain.pddl"
 problem_file = "./pddl/problem.pddl"
 solution_file = "./pddl/problem.pddl.soln"
 
 
 def main():
-    generate_task_file()
+    generate_problem_file()
     os.system(f"pyperplan {domain_file} {problem_file}")
     parse_solution_file()
 
 
-def getString_task_file_prefix():
+def getString_problem_file_prefix():
     prefix = f"""(define (problem pb_logistics)
     (:domain logistics)
     
@@ -27,7 +29,7 @@ def getString_task_file_prefix():
     return prefix
 
 
-def getString_task_file_init_prefix():
+def getString_problem_file_init_prefix():
     prefix = f"""
     (:init
         (at b loc_{ROBOT_POS[0]}_{ROBOT_POS[1]})
@@ -35,12 +37,12 @@ def getString_task_file_init_prefix():
     return prefix
 
 
-def getString_task_file_init_endfix():
+def getString_problem_file_init_endfix():
     endfix = f"""\t)\n"""
     return endfix
 
 
-def getString_task_file_endfix():
+def getString_problem_file_endfix():
     endfix = f"""
     (:goal
         (and (at b loc_{ROBOT_POS[0]}_{ROBOT_POS[1]}) (has_gold))
@@ -49,27 +51,28 @@ def getString_task_file_endfix():
     return endfix
 
 
-def getString_task_file_tiles():
+def getString_problem_file_tiles():
     prefix = "\t\t"
     endfix = "- tiles \n \t)\n"
     result = prefix
-    for row_id in range(GRID_WORLD_WIDTH + 1):
-        for col_id in range(GRID_WORLD_HEIGHT + 1):
+    for row_id in range(GRID_WORLD_X_MIN, GRID_WORLD_X_MAX + 1):
+        for col_id in range(GRID_WORLD_Y_MIN, GRID_WORLD_Y_MAX + 1):
             result = result + f"loc_{row_id}_{col_id} "
     result = result + endfix
     return result
 
 
 def is_within_grid_world(x, y):
-    if 0 <= x <= GRID_WORLD_WIDTH and 0 <= y <= GRID_WORLD_HEIGHT:
+    if GRID_WORLD_X_MIN <= x <= GRID_WORLD_X_MAX and \
+            GRID_WORLD_Y_MIN <= y <= GRID_WORLD_Y_MAX:
         return True
     return False
 
 
 def getString_right_tiles():
     result = "\t\t"
-    for x in range(GRID_WORLD_WIDTH + 1):
-        for y in range(GRID_WORLD_HEIGHT + 1):
+    for x in range(GRID_WORLD_X_MIN, GRID_WORLD_X_MAX + 1):
+        for y in range(GRID_WORLD_Y_MIN, GRID_WORLD_Y_MAX + 1):
             if is_within_grid_world(x, y + 1):
                 result = result + f"(right loc_{x}_{y} loc_{x}_{y + 1}) "
     result = result + "\n"
@@ -78,8 +81,8 @@ def getString_right_tiles():
 
 def getString_left_tiles():
     result = "\t\t"
-    for x in range(GRID_WORLD_WIDTH + 1):
-        for y in range(GRID_WORLD_HEIGHT + 1):
+    for x in range(GRID_WORLD_X_MIN, GRID_WORLD_X_MAX + 1):
+        for y in range(GRID_WORLD_Y_MIN, GRID_WORLD_Y_MAX + 1):
             if is_within_grid_world(x, y - 1):
                 result = result + f"(left loc_{x}_{y} loc_{x}_{y - 1}) "
     result = result + "\n"
@@ -88,8 +91,8 @@ def getString_left_tiles():
 
 def getString_down_tiles():
     result = "\t\t"
-    for x in range(GRID_WORLD_WIDTH + 1):
-        for y in range(GRID_WORLD_HEIGHT + 1):
+    for x in range(GRID_WORLD_X_MIN, GRID_WORLD_X_MAX + 1):
+        for y in range(GRID_WORLD_Y_MIN, GRID_WORLD_Y_MAX + 1):
             if is_within_grid_world(x + 1, y):
                 result = result + f"(down loc_{x}_{y} loc_{x + 1}_{y}) "
     result = result + "\n"
@@ -98,27 +101,27 @@ def getString_down_tiles():
 
 def getString_up_tiles():
     result = "\t\t"
-    for x in range(GRID_WORLD_WIDTH + 1):
-        for y in range(GRID_WORLD_HEIGHT + 1):
+    for x in range(GRID_WORLD_X_MIN, GRID_WORLD_X_MAX + 1):
+        for y in range(GRID_WORLD_Y_MIN, GRID_WORLD_Y_MAX + 1):
             if is_within_grid_world(x - 1, y):
                 result = result + f"(up loc_{x}_{y} loc_{x - 1}_{y}) "
     result = result + "\n"
     return result
 
 
-def generate_task_file():
+def generate_problem_file():
     content = ""
-    content = content + getString_task_file_prefix()
-    content = content + getString_task_file_tiles()
-    content = content + getString_task_file_init_prefix()
+    content = content + getString_problem_file_prefix()
+    content = content + getString_problem_file_tiles()
+    content = content + getString_problem_file_init_prefix()
 
     content = content + getString_right_tiles()
     content = content + getString_left_tiles()
     content = content + getString_down_tiles()
     content = content + getString_up_tiles()
 
-    content = content + getString_task_file_init_endfix()
-    content = content + getString_task_file_endfix()
+    content = content + getString_problem_file_init_endfix()
+    content = content + getString_problem_file_endfix()
 
     f = open(problem_file, "w")
     f.write(content)
